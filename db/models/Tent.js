aab47e3384d97e71977024f7a5a59a29552cd5c3
Priya <pitlapriya4u@gmail.com>
2026-06-10 18:00:20 +0200

    add Tent model and seed database with 6 tents

---
+++ db/models/Tent.js
@@ -0,0 +1,40 @@
+import { type } from "@testing-library/user-event/dist/cjs/utility/type.js";
+import mongoose, { Mongoose } from "mongoose";
+
+const TentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Indoor", "Outdoor"],
+    required: true,
+  },
+  brewery: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true,
+  },
+  pricePerPerson: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default mongoose.models.Tent || mongoose.model("Tent", TentSchema);
